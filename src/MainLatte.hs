aece97b63d6a8bc4eac752b1986ed3823aa167a1
pwojcieszak <piotrekw1002@gmail.com>
2024-12-11 13:46:51 +0100

    Divided optimisations and backend

--- src/MainLatte.hs
+++ src/MainLatte.hs
@@ -3,7 +3,7 @@ module Main where
 import Prelude
   ( ($), (.)
   , Either(..)
-  , Int, (>)
+  , Int, (>), Bool(True)
   , String, (++), concat, unlines
   , Show, show
   , IO, (>>), (>>=), mapM_, putStrLn
@@ -14,14 +14,19 @@ import Prelude
 import System.Environment ( getArgs )
 import System.Exit        ( exitFailure )
 import Control.Monad      ( when )
+import System.IO ( writeFile, hPutStrLn, stderr )
+import System.Process     ( callCommand )
+import System.Directory   ( createDirectoryIfMissing )
+import System.FilePath    ( takeBaseName, takeDirectory )
 
 import AbsLatte   ( Program(..) )
 import LexLatte   ( Token, mkPosToken )
 import ParLatte   ( pProgram, myLexer )
 import Frontend  ( checkSemantics )
-import System.IO (hPutStrLn, stderr)
 import Backend (generateLLVM)
+import Optimizations (optimizeProgram)
 import PrintLatte (Print, printTree)
+
 
 type Err        = Either String
 type ParseFun a = [Token] -> Err a
@@ -31,10 +36,10 @@ putStrV :: Verbosity -> String -> IO ()
 putStrV v s = when (v > 1) $ hPutStrLn stderr s
 
 runFile :: Verbosity -> ParseFun Program -> FilePath -> IO ()
-runFile v p f = readFile f >>= run v p
+runFile v p f = readFile f >>= run v p f
 
-run :: Verbosity -> ParseFun Program -> String -> IO ()
-run v p s =
+run :: Verbosity -> ParseFun Program -> FilePath -> String -> IO ()
+run v p f s =
   case p ts of
     Left err -> do
       hPutStrLn stderr "ERROR\nParse Failed...\n"
@@ -50,7 +55,19 @@ run v p s =
           exitFailure
         Right _ -> do
           hPutStrLn stderr "OK\n"
-          showTree v (generateLLVM tree)
+          let optimizedTree = optimizeProgram tree
+          let llvmCode = generateLLVM optimizedTree
+          let baseName = takeBaseName f
+          let outputDir = takeDirectory f
+          createDirectoryIfMissing True outputDir
+
+          let llFilePath = outputDir ++ "/" ++ baseName ++ ".ll"
+          let bcFilePath = outputDir ++ "/" ++ baseName ++ ".bc"
+
+          writeFile llFilePath llvmCode
+          -- callCommand $ "llvm-as " ++ llFilePath ++ " -o " ++ bcFilePath
+
+          showTree v optimizedTree
   where
   ts = myLexer s
   showPosToken ((l,c),t) = concat [ show l, ":", show c, "\t", show t ]
@@ -61,12 +78,19 @@ showTree v tree = do
   -- putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
   putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
 
+usage :: IO ()
+usage = do
+  putStrLn $ unlines
+    [ "usage: Call with one of the following argument combinations:"
+    , "  (files)         Parse and compile content of files verbosely."
+    , "  -s (files)      Silent mode. Parse and compile content of files silently."
+    ]
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    []         -> getContents >>= run 2 pProgram
+    []         -> usage
     "-s":fs    -> mapM_ (runFile 0 pProgram) fs
     fs         -> mapM_ (runFile 2 pProgram) fs
 
